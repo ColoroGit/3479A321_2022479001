64344084cfcc2ae4967e380d2fd35c63b17ace60
ColoroGit <113558258+ColoroGit@users.noreply.github.com>
2024-09-05 09:08:35 -0400

    Comments deleted

--- lab_1/lib/main.dart
+++ lab_1/lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     var logger = Logger();
@@ -20,27 +19,15 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         useMaterial3: true,
-
-        // Define the default brightness and colors.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.purple,
-          // TRY THIS: Change to "Brightness.light"
-          //           and see that all colors change
-          //           to better contrast a light background.
           brightness: Brightness.dark,
         ),
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
         textTheme: TextTheme(
           displayLarge: const TextStyle(
             fontSize: 72,
             fontWeight: FontWeight.bold,
           ),
-          // TRY THIS: Change one of the GoogleFonts
-          //           to "lato", "poppins", or "lora".
-          //           The title uses "titleLarge"
-          //           and the middle text uses "bodyMedium".
           titleLarge: GoogleFonts.oswald(
             fontSize: 30,
             fontStyle: FontStyle.italic,
